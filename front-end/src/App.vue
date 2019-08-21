f49c7259d0963a2da64fdb6bd8323cf813e44500
WavesLi <33248674+WavesLi@users.noreply.github.com>
2019-08-21 23:36:31 +0800

    web-CSS learn
    web 样式初步接触学习

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,38 +1,112 @@
 <template>
     <div>
-      <span>{{ serverResponse }} </span>
-      <button @click="getData">GET DATA</button>
+        <el-container>
+            <el-header class="header">Header
+                <!--<div>Header</div>-->
+            </el-header>
+            <el-main class="main">
+                <el-form ref="form" :model="form" label-width="80px">
+                    <el-form-item label="活动名称">
+                        <el-input v-model="form.name"></el-input>
+                    </el-form-item>
+                    <el-form-item label="活动区域">
+                        <el-select v-model="form.region" placeholder="请选择活动区域">
+                            <el-option label="区域一" value="shanghai"></el-option>
+                            <el-option label="区域二" value="beijing"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="活动时间">
+                        <el-col :span="11">
+                            <el-date-picker type="date" placeholder="选择日期" v-model="form.date1"
+                                            style="width: 100%;"></el-date-picker>
+                        </el-col>
+                        <el-col class="line" :span="2">-</el-col>
+                        <el-col :span="11">
+                            <el-time-picker placeholder="选择时间" v-model="form.date2"
+                                            style="width: 100%;"></el-time-picker>
+                        </el-col>
+                    </el-form-item>
+                    <el-form-item label="即时配送">
+                        <el-switch v-model="form.delivery"></el-switch>
+                    </el-form-item>
+                    <el-form-item label="活动性质">
+                        <el-checkbox-group v-model="form.type">
+                            <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
+                            <el-checkbox label="地推活动" name="type"></el-checkbox>
+                            <el-checkbox label="线下主题活动" name="type"></el-checkbox>
+                            <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
+                        </el-checkbox-group>
+                    </el-form-item>
+                    <el-form-item label="特殊资源">
+                        <el-radio-group v-model="form.resource">
+                            <el-radio label="线上品牌商赞助"></el-radio>
+                            <el-radio label="线下场地免费"></el-radio>
+                        </el-radio-group>
+                    </el-form-item>
+                    <el-form-item label="活动形式">
+                        <el-input type="textarea" v-model="form.desc"></el-input>
+                    </el-form-item>
+                     <el-form-item label="电话号码">
+                        <el-input v-model="form.phone"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" @click="onSubmit">立即创建</el-button>
+                        <el-button>取消</el-button>
+                    </el-form-item>
+                </el-form>
+            </el-main>
+            <el-footer class="footer">Footer</el-footer>
+        </el-container>
     </div>
 </template>
 
+
 <script>
-    import axios from 'axios';
-
     export default {
-      name: "my-first-vue",
-      data: function() {
-          return {
-            serverResponse: 'resp'
-          }
-      },
-      methods: {
-        getData() {
-          var that = this;
-          // 对应 Python 提供的接口，这里的地址填写下面服务器运行的地址，本地则为127.0.0.1，外网则为 your_ip_address
-          const path = 'http://127.0.0.1:5000/getMsg/';
-          axios.get(path).then(function (response) {
-            // 这里服务器返回的 response 为一个 json object，可通过如下方法需要转成 json 字符串
-            // 可以直接通过 response.data 取key-value
-            // 坑一：这里不能直接使用 this 指针，不然找不到对象
-            var msg = response.data.msg;
-            // 坑二：这里直接按类型解析，若再通过 JSON.stringify(msg) 转，会得到带双引号的字串
-            that.serverResponse = msg;
-
-            alert('Success ' + response.status + ', ' + response.data + ', ' + msg);
-          }).catch(function (error) {
-            alert('Error ' + error);
-          })
-        }
+        data() {
+            return {
+                form: {
+                    name: '',
+                    region: '',
+                    date1: '',
+                    date2: '',
+                    delivery: false,
+                    type: [],
+                    resource: '',
+                    desc: '',
+                    phone: ''
+                }
+            }
+        },
+        methods: {
+            onSubmit() {
+                console.log('submit!')
+            }
         }
     }
 </script>
+
+
+<style type="text/css">
+    body{margin: 0px;
+        padding: 0px;}
+    .header {
+        background-color: cornflowerblue;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .main {
+        background-color: #000;
+        color: black;
+        opacity: 0.8;
+
+    }
+
+    .footer {
+        background-color: cadetblue;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
